bd3be198535274ac37d04d1153d991eb831089d7
褚红彪 <hujiao_chuhongbiao@163.com>
2025-09-29 16:14:02 +0800

    develop:新建AI万能创作助手工程。

---
+++ AIUniversalAssistant/AIUniversalAssistant/AIUniversalAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIUniversalAssistantApp.swift
+//  AIUniversalAssistant
+//
+//  Created by 褚红彪 on 2025/9/29.
+//
+
+import SwiftUI
+
+@main
+struct AIUniversalAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIUniversalAssistant/AIUniversalAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIUniversalAssistant
+//
+//  Created by 褚红彪 on 2025/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
